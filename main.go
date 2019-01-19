b6767eeb4193f739a21747b82dff7bac4b602c76
onionpower <chocoonion@gmail.com>
2019-01-19 22:01:16 +0300

    random words guessing

--- main.go
+++ main.go
@@ -6,19 +6,21 @@ import (
 	"log"
 	"math/rand"
 	"net/http"
-	"strconv"
+	"strings"
 	"time"
 )
 
 var upgrader = websocket.Upgrader{}
 var word string
+var runeSet []rune
 
 func main() {
 	upgrader.CheckOrigin = func(r *http.Request) bool { return true }
 	fmt.Println("server is starting")
 	defer fmt.Println("server is stopped")
 	rand.Seed(time.Now().UnixNano())
-	word = newWord()
+	genWordsPool()
+	word = newWord(randLen())
 	http.HandleFunc("/", serve)
 	_ = http.ListenAndServe(":8080", nil)
 	_, _ = fmt.Scanln()
@@ -36,13 +38,15 @@ func serve(w http.ResponseWriter, r *http.Request) {
 		}
 		var resp []byte
 		switch string(message) {
+		case "HINT":
+			resp = []byte(word)
 		case "CONNECT":
 			resp = []byte(fmt.Sprintf("new: %v", len(word)))
 		case "EXIT":
 			break
 		default:
 			if word == string(message) {
-				word = newWord()
+				word = newWord(randLen())
 				resp = []byte(fmt.Sprintf("%v is right. new: %v", string(message), len(word)))
 			} else {
 				resp = []byte("wrong")
@@ -56,6 +60,20 @@ func serve(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func newWord() string {
-	return strconv.Itoa(rand.Intn(10))
+func randLen() int {
+	return rand.Intn(5) + 3
+}
+
+func newWord(wLen int) string {
+	w := strings.Builder{}
+	for ; wLen > 0; wLen-- {
+		w.WriteRune(runeSet[rand.Intn(len(runeSet))])
+	}
+	return w.String()
+}
+
+func genWordsPool() {
+	for r := 'a'; r <= 'z'; r++ {
+		runeSet = append(runeSet, r)
+	}
 }
