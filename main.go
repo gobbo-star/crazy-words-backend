bf30437ea07e8c24c3e4707977b759e610d62187
onionpower <chocoonion@gmail.com>
2019-01-19 21:51:17 +0300

    digit guessing

--- main.go
+++ main.go
@@ -1,25 +1,24 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"github.com/gorilla/websocket"
 	"log"
 	"math/rand"
 	"net/http"
+	"strconv"
 	"time"
 )
 
 var upgrader = websocket.Upgrader{}
-var rooms map[string]Room
+var word string
 
 func main() {
 	upgrader.CheckOrigin = func(r *http.Request) bool { return true }
 	fmt.Println("server is starting")
-	rooms = make(map[string]Room, 0)
-	rand.Seed(time.Now().UnixNano())
-
 	defer fmt.Println("server is stopped")
+	rand.Seed(time.Now().UnixNano())
+	word = newWord()
 	http.HandleFunc("/", serve)
 	_ = http.ListenAndServe(":8080", nil)
 	_, _ = fmt.Scanln()
@@ -37,15 +36,17 @@ func serve(w http.ResponseWriter, r *http.Request) {
 		}
 		var resp []byte
 		switch string(message) {
-		case "CREATE_ROOM":
-			roomName := fmt.Sprintf("r-%v", rand.Intn(5))
-			room := Room{}
-			rooms[roomName] = room
-			resp = []byte(roomName)
-		case "GET_ROOMS":
-			resp, _ = json.Marshal(rooms)
+		case "CONNECT":
+			resp = []byte(fmt.Sprintf("new: %v", len(word)))
+		case "EXIT":
+			break
 		default:
-			resp = []byte("UNKNOWN SIG")
+			if word == string(message) {
+				word = newWord()
+				resp = []byte(fmt.Sprintf("%v is right. new: %v", string(message), len(word)))
+			} else {
+				resp = []byte("wrong")
+			}
 		}
 		err = ws.WriteMessage(websocket.TextMessage, resp)
 		if err != nil {
@@ -53,4 +54,8 @@ func serve(w http.ResponseWriter, r *http.Request) {
 			break
 		}
 	}
+}
+
+func newWord() string {
+	return strconv.Itoa(rand.Intn(10))
 }
