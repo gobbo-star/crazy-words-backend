5ff90def8250430ebba8cd197e2fb544d6e4e843
onionpower <chocoonion@gmail.com>
2019-02-10 18:27:07 +0300

    color-gen-fix

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ PREV_VER=$CRW_TAG_VERSION
 CUR_VER=$(($PREV_VER+1))
 PREV_TAG=crw-$PREV_VER
 CUR_TAG=crw-$CUR_VER
-docker ps | grep $PREV_TAG | awk '{print $1}' | xargs docker stop
-docker build --tag=$CUR_TAG .
-docker run -v /usr/share/dict:/dict -p 8099:8080 -d $CUR_TAG /app/main -words /dict/words -colors list-of-colors -animals animals-list
+docker ps | grep crw | awk '{print $1}' | xargs docker stop
+docker build --tag=$CUR_TAG --tag=crw:latest .
+docker run -v /usr/share/dict:/dict -p 8099:8080 -d crw /app/main -words /dict/words -colors list-of-colors -animals animals-list
 export CRW_TAG_VERSION=$CUR_VER
